1416ca0c86230d3d8df42f51ffd9ce19fa5b0fbb
Olli Glorioso <84347872+olliglorioso@users.noreply.github.com>
2025-03-11 16:32:44 +0200

    Update run.sh

--- bag_SLAM_stats/run.sh
+++ bag_SLAM_stats/run.sh
@@ -7,16 +7,8 @@ TOPICS=(
     "/fmu/out/sensor_combined"
     "/fmu/out/vehicle_attitude"
     "/fmu/out/vehicle_gps_position"
-    "/zed_node/left_raw/image_raw_color"
-    "/zed_node/right_raw/image_raw/_color
-    "/zed_node/left_gray/image_rect_gray"
-    "/zed_node/right_gray/image_rect_gray"
-    "/zed_node/left/image_rect_color"
-    "/zed_node/right/image_rect_color"
-    "/zed_node/left_gray/image_rect_gray"
-    "/zed_node/right_gray/image_rect_gray"
-    "/zed_node/left/image_rect_color"
-    "/zed_node/right/image_rect_color"
+    "/zed_node/left_raw_gray/image_raw_gray"
+    "/zed_node/right_raw_gray/image_raw_gray"
     "/zed_node/left_gray/camera_info"
     "/zed_node/right_gray/camera_info"
 )
