2788e63c3faaed414eb3d89fcd79af24112636cc
Imran Mamin <imran@mm.st>
2025-04-20 22:34:22 +0300

    add calibration and centering to the ball_finder

--- ros2_ws/src/px4_handler/px4_handler/src/py/ball_finder.py
+++ ros2_ws/src/px4_handler/px4_handler/src/py/ball_finder.py
@@ -7,6 +7,7 @@ from rclpy.qos import QoSProfile, ReliabilityPolicy, HistoryPolicy, DurabilityPo
 from vision_msgs.msg import Detection2DArray
 import numpy as np
 import time
+import math
 
 
 class Maneuver(Node):
@@ -21,21 +22,23 @@ class Maneuver(Node):
         )
         self.vehicle_local_position_subscriber = self.create_subscription(VehicleLocalPosition, '/fmu/out/vehicle_local_position', self.vehicle_local_position_callback, qos_profile)
         self.trajectory_pub = self.create_publisher(TrajectorySetpoint, '/custom_trajectory', 10)
-        self.current_coords = np.array([0.0, 0.0, 0.0])
+        self.coords = np.array([0.0, 0.0, 0.0])
         self.detection_subscriber = self.create_subscription(Detection2DArray, '/detections', self.ball_detection_callback, 10)
         self.current_yaw = 0.0
         self.break_time = 5.0
         self.something_detected = False
         self.goto_rescue = False
-        self.rescue_mode = True
+        self.rescue_mode = False
 
-        # For follower mode
-        # self.direction_pub = self.create_publisher(String, '/direction_commands', 10)  # New topic for direction commands
-        # self.direction_subscriber = self.create_subscription(String, '/direction_commands', self.direction_callback, 10)  # Subscription to direction commands
+        self.ball_center_x = None
+        self.ball_center_y = None
 
+        self.image_height = 720 # change manually depending on our camera
+        self.image_width = 1280
+        
     
     def vehicle_local_position_callback(self, msg):
-        self.current_coords = np.array([msg.x, msg.y, msg.z])
+        self.coords = np.array([msg.x, msg.y, msg.z])
         self.current_yaw = msg.heading
     
     def ball_detection_callback(self, msg):
@@ -46,8 +49,11 @@ class Maneuver(Node):
                     if hypothesis.hypothesis.class_id == "32.0" and self.something_detected == False:  # Check for class_id 32
                         self.get_logger().info("Object with class_id 32 detected!")
                         self.something_detected = True
-                        return
+                self.ball_center_x = detection.bbox.center.position.x
+                self.ball_center_y = detection.bbox.center.position.y
+                return
         except Exception:
+            self.get_logger().info("Exception")
             return
 
 
@@ -57,22 +63,24 @@ class Maneuver(Node):
         msg.yaw = yaw
         self.trajectory_pub.publish(msg)
 
-    def move_to_waypoint(self, target_coords, yaw, speed=1.0, step_size=0.1, tolerance=0.2):
+    def move_to_waypoint(self, target_coords, yaw, speed=4.0, step_size=0.1, tolerance=0.2):
         target_coords = np.array(target_coords)
-        
-        while np.linalg.norm(self.current_coords - target_coords) > tolerance:
-            direction = target_coords - self.current_coords
+        ramp_factor = 0.70  # Start at 70% speed
+        ramp_increment = 0.1  # How fast to ramp up
+        max_ramp = 1.0  # Cap at full speed
+
+        while np.linalg.norm(self.coords - target_coords) > tolerance:
+            if self.something_detected and not self.goto_rescue:
+                return
+            direction = target_coords - self.coords
             distance = np.linalg.norm(direction)
-            if self.something_detected and self.goto_rescue == False:
-                return 
             if distance > 0:
                 step = direction / distance * speed * step_size 
-                self.current_coords += step 
+                self.coords += step * ramp_factor
 
-            self.publish_trajectory(self.current_coords[0], self.current_coords[1], self.current_coords[2], yaw)
-            #self.get_logger().info(str([self.current_coords[0], self.current_coords[1], self.current_coords[2]]))
-            
-            rclpy.spin_once(self, timeout_sec=0.05)
+            self.publish_trajectory(self.coords[0], self.coords[1], self.coords[2], yaw)
+            ramp_factor = min(max_ramp, ramp_factor + ramp_increment)
+            rclpy.spin_once(self, timeout_sec=self.break_time)
             
     def rotate(self, yaw, speed=1.0, step_size=0.1, tolerance=0.2):
         while abs(self.current_yaw - yaw) > tolerance:
@@ -80,7 +88,7 @@ class Maneuver(Node):
             step = direction * speed * step_size
             self.current_yaw += step
             
-            self.publish_trajectory(self.current_coords[0], self.current_coords[1], self.current_coords[2], self.current_yaw)
+            self.publish_trajectory(self.coords[0], self.coords[1], self.coords[2], self.current_yaw)
             self.get_logger().info(f"Yaw: {self.current_yaw:.2f}")
             time.sleep(step_size)  
             
@@ -94,14 +102,52 @@ class Maneuver(Node):
             time.sleep(1)
             if self.something_detected:
                 break
-        
-        # self.rotate(6.28, speed=speed)
-        # self.rotate(0.0, speed=speed)
-    
-    def start_moving(self):
-        time.sleep(5)
+
+
+    def centralize(self):
+        img_x = self.image_width / 2.0
+        img_y = self.image_height / 2.0
+        init_x = self.ball_center_x
+        init_y = self.ball_center_y
+
+        x, y, z = self.coords[0], self.coords[1], self.coords[2]
+       
+        print("Calibrating")
+        self.move_to_waypoint([x + 2, y + 2, z], self.current_yaw)
         rclpy.spin_once(self, timeout_sec=self.break_time)
-        x, y, z = self.current_coords[0], self.current_coords[1], self.current_coords[2] # hardcode z
+
+        
+        
+        time.sleep(2)
+        ball_dx = self.ball_center_x - init_x
+        ball_dy = self.ball_center_y - init_y
+        theta_rad = math.atan2(ball_dy, ball_dx)
+        theta_deg = math.degrees(theta_rad)
+        x2, y2, z2 = self.coords[0], self.coords[1], self.coords[2]
+        print(f"Image X axis is rotated {theta_deg:.2f}° from VLP.x")
+        offset_x = img_x - self.ball_center_x
+        offset_y = img_y - self.ball_center_y
+        norm_i = math.sqrt(offset_x**2 + offset_y**2)
+        ix, iy = [- offset_x / norm_i, - offset_y / norm_i]
+        print(ix, iy, " ix and iy before transformation")
+        vlp_dx = ix * math.cos(theta_rad) - iy * math.sin(theta_rad)
+        vlp_dy = ix * math.sin(theta_rad) + iy * math.cos(theta_rad)
+
+        offset_vec_vlp = [vlp_dx, vlp_dy]
+        print(offset_vec_vlp, " drone movement")
+        target_x = x2 + vlp_dx * 10
+        target_y = y2 + vlp_dy * 10
+        # Command the drone to move to this target position
+        self.move_to_waypoint([target_x, target_y, z2], self.current_yaw)
+
+
+        print("Centralized")
+        return
+
+    def start_moving(self):
+        time.sleep(2)
+        rclpy.spin_once(self, timeout_sec=self.break_time)
+        x, y, z = self.coords[0], self.coords[1], self.coords[2] # hardcode z
         yaw = self.current_yaw
 
         waypoints = [
@@ -121,9 +167,11 @@ class Maneuver(Node):
         if self.something_detected:
             if self.rescue_mode:
                 self.goto_rescue = True
-                self.move_to_waypoint([self.current_coords[0], self.current_coords[1], -5.0], self.current_yaw, s1)
+                self.move_to_waypoint([self.coords[0], self.coords[1], -5.0], self.current_yaw, s1)
             else:
                 print("Follower mode")
+                self.goto_rescue = True
+                self.centralize()
 
 
 def main(args=None):
