910f7d0b83550e5a136ca2cc59437cab51661245
Olli Glorioso <84347872+olliglorioso@users.noreply.github.com>
2025-05-05 19:30:15 +0300

    moi

--- ros2_ws/src/px4_handler/scripts/ball_finder.py
+++ ros2_ws/src/px4_handler/scripts/ball_finder.py
@@ -66,6 +66,11 @@ class Maneuver(Node):
                         if not self.something_detected:
                             self.get_logger().info("Object with class_id 32 detected!")
                             self.something_detected = True
+                self.ball_center_x = detection.bbox.center.position.x
+                self.ball_center_y = detection.bbox.center.position.y
+                if not self.something_detected:
+                    self.get_logger().info("Object with class_id 32 detected!")
+                    self.something_detected = True
 
                 return
         except Exception:
