0ea02657b23a6a007ce1a70c84d5c738f2093ecd
Olli Glorioso <olli.glorioso8@gmail.com>
2025-12-17 19:23:29 +0200

    latest state

--- ros2_ws/src/tmux_autostart.sh
+++ ros2_ws/src/tmux_autostart.sh
@@ -19,13 +19,13 @@ tmux send-keys -t $SESSION:0.2 'bash -c "source install/setup.bash && ros2 run p
 # Pane 3
 tmux select-pane -t $SESSION:0.2
 tmux split-window -v -t $SESSION
-tmux send-keys -t $SESSION:0.3 'bash -c "source install/setup.bash && ros2 run vision_package object_detector.py"' C-m
+tmux send-keys -t $SESSION:0.3 'bash -c "source install/setup.bash && ros2 run vision_package object_detector.py --ros-args -p output_topic_name:=\"detections\""' C-m
 
 
 # Pane 3
-tmux select-pane -t $SESSION:0.3
-tmux split-window -v -t $SESSION
-tmux send-keys -t $SESSION:0.4 'bash -c "source install/setup.bash && ros2 run rqt_image_view rqt_image_view"' C-m
+#tmux select-pane -t $SESSION:0.3
+#tmux split-window -v -t $SESSION
+#tmux send-keys -t $SESSION:0.4 'bash -c "source install/setup.bash && ros2 run rqt_image_view rqt_image_view"' C-m
 
 # Layout
 tmux select-layout -t $SESSION tiled
