c899ea997b3620f6c5d58ced6a24a1b77254ba44
King Peeko <erik.walo@gmail.com>
2026-02-10 15:03:45 +0200

    feat: Rename first_standalone.py and add stereo camera

--- simulation/IsaacSim/standalones/isaac_simple_obstacles.py
+++ simulation/IsaacSim/standalones/isaac_simple_obstacles.py
@@ -11,6 +11,7 @@ simulation_app = SimulationApp({
 
 import omni.timeline
 from omni.isaac.core.world import World
+from omni.isaac.core.utils.rotations import euler_angles_to_quat
 from scipy.spatial.transform import Rotation
 
 # Import the Pegasus API
@@ -73,7 +74,42 @@ class PegasusApp:
                 "orientation": np.array([180, -90.0, 0.0]),
             }
         )
-        config_multirotor.graphical_sensors = [down_facing_camera]
+
+        # ZED Stereo Camera - Left Camera
+        stereo_left_camera = MonocularCamera(
+            camera_name="zed_left_camera",
+            config={
+                "update_rate": 30.0,
+                "intrinsics": np.array(
+                    [[1049.7, 0.0, 960.0], [0.0, 1049.7, 540.0], [0.0, 0.0, 1.0]]
+                ),
+                "diagonal_fov": 100.0, 
+                "resolution": (1920, 1080),
+                "position": np.array([0.10, -0.06, 0.0]),  # Front of drone, left side
+                "orientation": np.array([0.0, 0.0, 180.0]),
+            }
+        )
+
+        # ZED Stereo Camera - Right Camera
+        stereo_right_camera = MonocularCamera(
+            camera_name="zed_right_camera",
+            config={
+                "update_rate": 30.0,
+                "intrinsics": np.array(
+                    [[1049.7, 0.0, 960.0], [0.0, 1049.7, 540.0], [0.0, 0.0, 1.0]]
+                ),
+                "diagonal_fov": 100.0,
+                "resolution": (1920, 1080),
+                "position": np.array([0.10, 0.06, 0.0]),  # Front of drone, right side
+                "orientation": np.array([0.0, 0.0, 180.0]),  # Front-facing, rotated to the right
+            }
+        )
+
+        config_multirotor.graphical_sensors = [
+            down_facing_camera,
+            stereo_left_camera,
+            stereo_right_camera
+            ]
 
 
         # Create and add a multirotor vehicle to the world
